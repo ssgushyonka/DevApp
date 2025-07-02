3fa3adad5b0905436efaa21b70178f03c5df7247
ssgushyonka <orangeee04@gmail.com>
2025-07-02 17:27:56 +0300

    Initial Commit

---
+++ DevApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevApp
+//
+//  Created by Элина Борисова on 02.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
